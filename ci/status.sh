58dc0058302dba222e01aed11edad693f6f126c4
Hideyuki Hirauchi <hirauchi@ideapump.net>
2018-03-23 13:17:39 +0900

    chore(ci/status): pr-check-okと入れ替わりで削除

--- ci/status.sh
+++
@@ -1,8 +0,0 @@
-TOKEN=$GIT_SQLPP_TOKEN
-OWNER=hirauchi0713
-REPO=sqlpp
-SHA=$1
-
-curl -H "Authorization: token $TOKEN" \
-  https://api.github.com/repos/$OWNER/$REPO/statuses/$SHA \
-  -X POST -d @./ci/status.json
