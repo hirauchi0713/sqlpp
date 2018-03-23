7e65593314e2a9efbe18c72d7b52cf619a50f745
Hideyuki Hirauchi <hirauchi@ideapump.net>
2018-03-23 14:20:55 +0900

    refactor(ci): ci.jsにmakeGetArgsなどを追加

--- ci/pr-check-ok.js
+++ ci/pr-check-ok.js
@@ -2,46 +2,27 @@
 // 指定されたプルリクにcheck okを出す
 //
 
-require('dotenv').config()
 const ci = require('./ci.js')
-
-const path = require('path')
 
 const Client = require('node-rest-client').Client
 const client = new Client()
 
-const OWNER  = process.env.OWNER
-const REPO   = process.env.REPO
-const TOKEN  = process.env.GIT_SQLPP_TOKEN
-const NUMBER = process.argv[2]
+const list_commits = 'https://api.github.com/repos/${OWNER}/${REPO}/pulls/${NUMBER}/commits'
+const statuses = 'https://api.github.com/repos/${OWNER}/${REPO}/statuses/'
 
-const list_commits = `https://api.github.com/repos/${OWNER}/${REPO}/pulls/${NUMBER}/commits`
-const statuses = `https://api.github.com/repos/${OWNER}/${REPO}/statuses/`
+var  getArgs  = ci.makeGetArgs()
+getArgs.path.NUMBER = process.argv[2]
 
-const args = {
- headers: {
-   "User-Agent": "node-rest-client",
-   "Authorization": `token ${TOKEN}`
- }
-}
+const postArgs = ci.makePostArgs({}, {
+  "state": "success",
+  "context": "check"
+})
 
-const postargs = {
-  headers: {
-    "User-Agent": "node-rest-client",
-    "Content-Type": "application/json",
-    "Authorization": `token ${TOKEN}`
-  },
-  data : {
-    "state": "success",
-    "context": "check"
-  }
-}
-
-client.get(list_commits, args, (json, res) => {
+client.get(list_commits, getArgs, (json, res) => {
   ci.responseLog(res)
   json.forEach(commits => {
     const url = statuses+commits.sha
-    client.post(url, postargs, (data, res) => {
+    client.post(url, postArgs, (data, res) => {
       ci.responseLog(res)
     })
   })
