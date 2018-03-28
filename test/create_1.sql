5f9a02e4a6c441b6621930f9cb75bf23144e3b37
Hideyuki Hirauchi <hirauchi@ideapump.net>
2018-03-29 07:57:42 +0900

    test(update): updateを追加。利用者にパスワードハッシュを追加

--- test/create_1.sql
+++ test/create_1.sql
@@ -1,16 +1,18 @@
 --- { section: '入力データ' }
-create table 利用者(id serial primary key, 氏名 text not null, 年齢 integer);
+create table 利用者(id serial primary key, 氏名 text not null, 年齢 integer, パスワードハッシュ text);
 
 --- { section: '期待する出力' }
 CREATE TABLE 利用者 (
   id SERIAL PRIMARY KEY,
   氏名 TEXT,
-  年齢 INTEGER
+  年齢 INTEGER,
+  パスワードハッシュ TEXT
 );
 
 --- { section: '期待する出力', option { valign: 1 } }
 CREATE TABLE 利用者 (
-  id   SERIAL PRIMARY KEY,
-  氏名 TEXT,
-  年齢 INTEGER
+  id                 SERIAL PRIMARY KEY,
+  氏名               TEXT,
+  年齢               INTEGER,
+  パスワードハッシュ TEXT
 );
